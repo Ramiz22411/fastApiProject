bc95267d555e0e7e2f9ef9b755646629e31ca3b0
ramiz22411 <iceonhone666@gmail.com>
2024-10-13 20:32:03 +0500

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,68 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+books = {
+    {
+        "id": 1,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 7,
+        "title": "Harry Potter",
+        "author": "<NAME>",
+        "publisher": "Harry Potter",
+        "publisher_date": "2021-01-010",
+        "pages_count": 1234,
+        "language": "English",
+    },
+}
